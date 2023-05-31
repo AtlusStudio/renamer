2431adf25d4a4c3999bfd047bc5e0427b84e7caf
NuthX <huermos@gmail.com>
2023-05-31 11:31:15 +0800

    新增listview列

--- test.py
+++ test.py
@@ -1,27 +1,14 @@
-import requests
+from multiprocessing import Process
+import os
 
-def get_chinese_name(anime_name):
-    url = f"https://myanimelist.net/search/all?q={anime_name}"
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36",
-    }
+# 子进程要执行的代码
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
 
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-
-    start_index = response.text.find('<a class="hoverinfo_trigger fw-b" href="') + len('<a class="hoverinfo_trigger fw-b" href="')
-    end_index = response.text.find('">', start_index)
-    anime_url = response.text[start_index:end_index]
-
-    response = requests.get(anime_url, headers=headers)
-    response.raise_for_status()
-
-    start_index = response.text.find('<span itemprop="name">') + len('<span itemprop="name">')
-    end_index = response.text.find('</span>', start_index)
-    chinese_name = response.text[start_index:end_index]
-
-    return chinese_name
-
-anime_name = input("请输入动画的英文名：")
-chinese_name = get_chinese_name(anime_name)
-print(f"动画的中文名是：{chinese_name}")
+if __name__=='__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
