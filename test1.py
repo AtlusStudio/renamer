6965ae0aeeaa176cb1eec5140379e2f5408eb10a
NuthX <huermos@gmail.com>
2023-06-01 00:03:27 +0800

    sync

--- test1.py
+++ test1.py
@@ -1,20 +1,3 @@
-import wx
+import wx.demo
 
-# 创建应用程序对象
-app = wx.App()
-
-# 创建顶级窗口
-frame = wx.Frame(None, title="标识示例")
-
-# 创建面板
-panel = wx.Panel(frame)
-
-# 创建标签
-label = wx.StaticText(panel, label="这是一个很长的文本标签，将被截断显示")
-label.Wrap(100)  # 设置换行宽度为100像素
-
-# 显示窗口
-frame.Show()
-
-# 进入应用程序主循环
-app.MainLoop()
+wx.demo.run()
