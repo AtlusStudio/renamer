c38be009ac8e6d0a4e75e08c3225072f75df0e32
NuthX <huermos@gmail.com>
2023-06-01 22:50:28 +0800

    Revert "修正代码样式"
    This reverts commit a164e7ffdc7e45dcaf351a7c89cfc5c074d62a74.

--- module/function.py
+++ module/function.py
@@ -2,7 +2,6 @@ import os
 import re
 
 from module import api
-from module import gui
 
 
 # 输入的动画文件夹 file_name，通过正则提取动画名 romaji_name
@@ -99,79 +98,10 @@ def get_anime_info(list_id, path):
     print(f"搜索完成，该动画第一季为{prev_name}")
     this_anime_dict["b_originate_name"] = prev_name
 
-
-    
-
-
-
-
-
-
-    # # 如果获得的 b_sid 与 b_id 不同，说明之前还有前传，则继续执行
-    # while b_temp_id != b_id:
-    #     print("当前轮次似乎有前传，正在获取前传ID")
-    #     bangumi_previous_result = api.bangumi_previous(b_temp_id, b_cn_name)
-    #     b_sid = str(bangumi_sid_result[0])
-    #     print(b_sid)
-    # else:
-    #     print("ok")
-
-    # print("该动画无前传")
-
-
-
-
-
-
-
-
     return this_anime_dict
 
 
-# 根据动画路径获取动画信息
-def start_analysis(self):
-    # 调用获取到的文件路径列表
-    file_path_exist = gui.list_ctrl.GetDropTarget().file_path_exist
 
-    # 判断列表是否为空
-    if file_path_exist == set():
-        print("请先拖入文件夹")
-        # 禁用按钮
-        # self.analysis_button.Enable(False)
-    else:
-        # 创建列表，写入所有抓取的数据
-        anime_list = []
-
-        # 循环开始：分析每个文件
-        list_id = 0
-        for file_path in file_path_exist:
-
-            # 通过文件路径 file_path 获取数据，合并入列表 anime_list
-            this_anime_dict = function.get_anime_info(list_id, file_path)
-            anime_list.append(this_anime_dict)
-            print(anime_list)
-
-            # 写入listview
-            # 如果没有 b_originate_name 说明没有执行到最后一步
-            # 重写 file_file 并展示在 listview 避免错位
-            this_anime = anime_list[list_id]
-            if "b_originate_name" in this_anime:
-                file_name = anime_list[list_id]["file_name"]
-                b_cn_name = anime_list[list_id]["b_cn_name"]
-                b_originate_name = anime_list[list_id]["b_originate_name"]
-                self.list_ctrl.SetItem(list_id, 0, file_name)
-                self.list_ctrl.SetItem(list_id, 1, b_cn_name)
-                self.list_ctrl.SetItem(list_id, 2, b_originate_name)
-            else:
-                print("该动画未获取到内容，已跳过")
-
-            # 进入下一轮前修改 ID
-            list_id += 1
-
-def on_clear_list(self, event):
-    self.list_ctrl.DeleteAllItems()
-    self.file_path_exist = []
-    print("已清除所有文件夹")
 
 
 
